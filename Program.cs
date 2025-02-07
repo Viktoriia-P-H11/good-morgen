1b0d5113e3e99560924ee794c93add2c9ebe127d
Viktoriia <viktoriia1ua@gmail.com>
2025-02-07 12:33:23 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+     DateTime now = DateTime.Now;
+        int hour = now.Hour;
+        
+        string greeting;
+        
+        if (hour >= 5 && hour < 12)
+        {
+            greeting = "Good morning!";
+        }
+        else if (hour >= 12 && hour < 18)
+        {
+            greeting = "Good afternoon!";
+        }
+        else if (hour >= 18 && hour < 22)
+        {
+            greeting = "Good evening!";
+        }
+        else
+        {
+            greeting = "Good night!";
+        }
+        
+        Console.WriteLine(greeting);
+    }
+}
+
